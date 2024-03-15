760f11b1f06bb4c5085ea6ac3e16c43b0daffc87
kmentz <kassiane.mentz@adp.com>
2024-03-15 20:01:06 -0300

    :corrigindo bug ao deletar quarto

--- src/main/java/com/fiap/Hackaton/usecase/quarto/usecases/DeletarQuartoUseCase.java
+++ src/main/java/com/fiap/Hackaton/usecase/quarto/usecases/DeletarQuartoUseCase.java
@@ -2,17 +2,22 @@ package com.fiap.Hackaton.usecase.quarto.usecases;
 
 import com.fiap.Hackaton.domain.quarto.entity.Quarto;
 import com.fiap.Hackaton.domain.quarto.gateway.QuartoGateway;
+import com.fiap.Hackaton.usecase.reserva.RemoverQuartoDeReservasUseCase;
 
 public class DeletarQuartoUseCase {
 
     private final QuartoGateway quartoGateway;
 
-    public DeletarQuartoUseCase(QuartoGateway quartoGateway) {
+    private final RemoverQuartoDeReservasUseCase removerQuartoDeReservasUseCase;
+
+    public DeletarQuartoUseCase(QuartoGateway quartoGateway, RemoverQuartoDeReservasUseCase removerQuartoDeReservasUseCase) {
         this.quartoGateway = quartoGateway;
+        this.removerQuartoDeReservasUseCase = removerQuartoDeReservasUseCase;
     }
 
     public void executar(Long id) {
         Quarto quarto = this.quartoGateway.buscarPorId(id).orElseThrow();
+        removerQuartoDeReservasUseCase.executar(quarto);
         this.quartoGateway.deletar(quarto);
     }
 }
